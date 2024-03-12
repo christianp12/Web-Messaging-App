ea6aab441009cbf457b9cfdbc5eff27326afa170
Christian Petruzzella <krys.petruz@yahoo.it>
2024-03-12 09:46:48 +0100

    Frontend & Backend

---
+++ Chat Up/backend/db.js
@@ -0,0 +1,9 @@
+const config = require('./config')
+const admin = require('firebase-admin');
+
+const db = admin.initializeApp({
+    credential: admin.credential.cert(config.key),
+    databaseURL: "https://progettoFondamentiWeb.firebaseio.com",
+});
+
+module.exports = db;
